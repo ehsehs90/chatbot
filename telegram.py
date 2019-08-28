fe2e9b988e86de0e63b83228124039003e2a8925
dohyun lee <ehgus1551@naver.com>
2019-08-28 17:30:24 +0900

    update telegram.py

---
+++ telegram.py
@@ -0,0 +1,26 @@
+"""
+python 으로 telegram message보내기
+
+"""
+
+import requests
+
+
+
+#(1) getUpdates 을 통해 chat_id를 가져옴
+id_url = 'https://api.telegram.org/bot872675185:AAHqLJuLIRJc6QGs-W85MDHONNKMClJkC5w/getUpdates'
+response =  requests.get(id_url)
+res_dict = response.json()
+chat_id = res_dict['result'][0]['message']['chat']['id']
+
+print(chat_id)
+# (2) url를 조합하여 requests로 요청 보내기
+
+token = '872675185:AAHqLJuLIRJc6QGs-W85MDHONNKMClJkC5w/'
+base_url = 'https://api.telegram.org/'
+msg = '555'
+method = 'sendMessage?'
+
+url = base_url +'bot'+ token + method + 'chat_id=' +str(chat_id) +'&text='+ msg
+
+requests.get(url)
